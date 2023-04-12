5bbe4401e95e2bdd3850c656fdfa5dd537532567
Naveen Manne <113809829+Nav1719@users.noreply.github.com>
2023-04-12 18:47:29 -0500

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/glass', glassRouter);
+app.use('/glasses', glassRouter);
 app.use('/board', boardRouter);
 app.use('/selector', selectorRouter);
 
